6c5a512b101196ff473b0fe442d5619548b95523
Alejandro Nanez <alejonanez@gmail.com>
2021-05-22 19:15:48 -0500

    Handle errors in the API layer.

--- data/api.js
+++ data/api.js
@@ -8,9 +8,33 @@ function getRandomInt(min, max) {
 
 export async function getRandomPhoto() {
   const randomPhotoId = getRandomInt(1, 10);
-  const photo = await fetch(
-    `https://jsonplaceholder.typicode.com/photos/${randomPhotoId}`
-  ).then((response) => response.json());
+  let photo;
+  try {
+    photo = await fetch(
+      `https://jsonplaceholder.typicode.com/photos/${randomPhotoId}`
+    ).then((response) => {
+      /**
+       * We consider any response not in the ranges of 200-299 as an
+       * invalid response.
+       * That covers 4xx errors and 5xx errors.
+       */
+      if (!response.ok) {
+        throw new Error("Something went wrong with the request");
+      }
+      return response.json();
+    });
+  } catch (e) {
+    /**
+     * We'll get to this block if:
+     * - Response was NOT OK.
+     * - We couldn't complete the request
+     *
+     * We should log whatever is coming from `e` to our
+     * reporting/alerting system (see Sentry.io)
+     */
+    console.log("...logging error to our system...");
+    throw e;
+  }
 
   return photo;
 }
