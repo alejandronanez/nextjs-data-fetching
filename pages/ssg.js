181c7b1596afa5e8a2572160f0cf46d6e39fef02
Alejandro Nanez <alejonanez@gmail.com>
2021-05-22 22:33:11 -0500

    Add SSG and SSR logic to /ssg and /ssg

--- pages/ssg.js
+++ pages/ssg.js
@@ -1,7 +1,41 @@
-function SSG() {
+import { getRandomPhoto } from "../data/api";
+
+function SSG({ photo }) {
   return (
-    <div>SSG</div>
-  )
+    <div>
+      <h1>{photo.title}</h1>
+      <img src={photo.url} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  let photo;
+  try {
+    photo = await getRandomPhoto();
+  } catch (e) {
+    /**
+     * If something goes wrong, we return a 404 page
+     */
+    return {
+      notFound: true,
+    };
+  }
+
+  if (!photo) {
+    /**
+     * If we don't get a `photo` back, we return a 404 page
+     */
+    return {
+      notFound: true,
+    };``
+  }
+
+  return {
+    props: {
+      photo,
+    },
+  };
 }
 
 export default SSG;
